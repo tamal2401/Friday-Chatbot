828c6911d29239373bd2e6d8bf5870ff13a10c95
Tamal <onlyjohn98@gmail.com>
2018-12-31 02:11:14 +0530

    changes 2

--- FriDay/src/main/java/com/java/Friday/main.java
+++ FriDay/src/main/java/com/java/Friday/main.java
@@ -1,6 +1,20 @@
 package com.java.Friday;
 
+import java.io.File;
+
 public class main {
+
+	private static final boolean TRACE_MODE = false;
+	static String botName = "super";
+
+	private static String getResourcesPath() {
+		File currDir = new File(".");
+		String path = currDir.getAbsolutePath();
+		path = path.substring(0, path.length() - 2);
+		System.out.println(path);
+		String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+		return resourcesPath;
+	}
 
 	public static void main(String[] args) {
 		helper help = new helper();
